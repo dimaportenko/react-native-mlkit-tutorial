96f64179e98f758542389d6635d15af30f1a6ad0
Dmytro Portenko <troublesohard@gmail.com>
2021-07-17 19:03:51 +0300

    create ios native module

---
+++ ios/MLKit/RCTTextRecognitionModule.h
@@ -0,0 +1,16 @@
+//
+//  RCTTextRecognitionModule.h
+//  RNMLKitTutorial
+//
+//  Created by Dmytro on 17.07.2021.
+//
+
+#ifndef RCTTextRecognitionModule_h
+#define RCTTextRecognitionModule_h
+
+#import <React/RCTBridgeModule.h>
+@interface RCTTextRecognitionModule : NSObject <RCTBridgeModule>
+
+@end
+
+#endif /* RCTTextRecognitionModule_h */
